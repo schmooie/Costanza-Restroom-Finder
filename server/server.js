a8f178ba3a04744b108a58210bb302b93f445e4a
Sam Lau <slau234@gmail.com>
2014-07-19 15:17:13 -0400

    Modified Mongo URI.

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var methodOverride = require('method-override');
 var restify = require('express-restify-mongoose');
 var http = require('http');
 
-mongoose.connect('mongodb://localhost/nycRestrooms');
+mongoose.connect('mongodb://mongo:mymongo@novus.modulusmongo.net:27017/n2osopaB');
 
 var Restroom = mongoose.model('Restroom', {
 	oid: Number,
