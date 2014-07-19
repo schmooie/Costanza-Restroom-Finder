6460358e95c50700a259ad4a2669ae69b62a239d
Eddie Ng <wards.ng@gmail.com>
2014-07-19 15:41:37 -0400

    Added geolocator service.

--- pottyParty/www/js/app.js
+++ pottyParty/www/js/app.js
@@ -1,9 +1,10 @@
-// Ionic Starter App
+// Potty Party - Bring the party to the potty, not your pants
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('pottyParty.factories', []);
+angular.module('pottyParty', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -16,4 +17,4 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
