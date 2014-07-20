67cb6daff0b267ff6fffb2f9cc27254b91e619f6
Sam Lau <slau234@gmail.com>
2014-07-20 00:08:55 -0400

    Added marker clusterers.

--- pottyParty/www/js/controllers/main.controller.js
+++ pottyParty/www/js/controllers/main.controller.js
@@ -1,64 +1,47 @@
 angular.module('pottyParty.controllers')
   .controller('MainCtrl', ['$scope', '$http', '$cordovaGeolocation', '$cordovaToast', 'mapFuncs', 'geocoder', 'storage',
     function($scope, $http, $cordovaGeolocation, $cordovaToast, mapFuncs, geocoder, storage) {
-
+      var mapCanvas;
       var mapOptions = {
         center: new google.maps.LatLng(40.754926, -73.984281),
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         styles: mapFuncs.style,
         zoom: 13
       };
-
-      var mapCanvas;
+      var markers = [];
       var allRestrooms = storage.getObject('allRestrooms');
-
-      var loadMarkers = function () {
-	      if (allRestrooms === null) {
-	        console.log('Requesting data from server...');
-	        $http.get('http://nycrestrooms.herokuapp.com/api/v1/Restrooms')
-	        	.success(function(restrooms){
-	        		allRestrooms = restrooms;
-	            storage.setObject('allRestrooms', allRestrooms);
-	         //    allRestrooms.forEach(function(el){
-	      			// 	mapFuncs.makeMarker(true, mapCanvas, el);
-	      			// });
-	        		console.log(allRestrooms);
-	        	}).error(function(err) {
-	            console.log('Failed to get restrooms: ', err);
-	          });
-	      } else {
-	        console.log('Loaded from localStorage: ', allRestrooms);
-	      }
-      };
-
-      var filterByRadius = function(origin, data, radius) {
-        // 0.0145 lat/lng
-        var c2 = radius*radius;
-        var xMin = origin.latitude - radius;
-        var xMax = origin.latitude + radius;
-        var yMin = origin.longitude - radius;
-        var yMax = origin.longitude + radius;
-        var filteredLocations = [];
-
-        data.forEach(function(place) {
-          if (place.coords[1] > xMax || place.coords[1] < xMin ||
-              place.coords[0] > yMax || place.coords[0] < yMin) {
-            return;
-          }
-          var a2 = (place.coords[1]-origin.latitude)*(place.coords[1]-origin.latitude);
-          var b2 = (place.coords[0]-origin.longitude)*(place.coords[0]-origin.longitude);
-          if (a2 + b2 <= c2) {
-            filteredLocations.push(place);
-          }
-        });
-
-        return filteredLocations;
-      };
 
       var initialize = function() {
         mapCanvas = new google.maps.Map(document.getElementById("map-canvas"),
           mapOptions);
         loadMarkers();
+      };
+
+      var loadMarkers = function() {
+        if (allRestrooms === null) {
+          console.log('Requesting data from server...');
+          $http.get('http://nycrestrooms.herokuapp.com/api/v1/Restrooms')
+            .success(function(restrooms) {
+              allRestrooms = restrooms;
+              storage.setObject('allRestrooms', allRestrooms);
+              //    allRestrooms.forEach(function(el){
+              // 	mapFuncs.makeMarker(true, mapCanvas, el);
+              // });
+              console.log(allRestrooms);
+            }).error(function(err) {
+              console.log('Failed to get restrooms: ', err);
+            });
+        } else {
+          console.log('Loaded from localStorage: ', allRestrooms);
+        }
+      };
+
+      var makeClusters = function() {
+        var mc = new MarkerClusterer(mapCanvas, markers, mcOptions);
+        var mcOptions = {
+          gridSize: 5,
+          maxZoom: 15
+        };
       };
 
       google.maps.event.addDomListener(window, 'load', initialize);
@@ -69,13 +52,14 @@ angular.module('pottyParty.controllers')
           var currentLocation = res.coords;
           var address = res.coords.latitude + ',' + res.coords.longitude;
 
-          var userLocation = new google.maps.LatLng(res.coords.latitude, res.coords.longitude);
-          mapCanvas.setCenter(userLocation);
+          var userLocationObj = new google.maps.LatLng(currentLocation.latitude, currentLocation.longitude);
+          // UNCOMMENT BELOW
+          // mapCanvas.setCenter(userLocationObj);
           mapCanvas.setZoom(15);
 
           // GEORGE COSTANZA
-          var costanza = mapFuncs.makeMarker(false, mapCanvas, userLocation, 'George Costanza');
-          // costanza.setMap(mapCanvas);
+          // temporary hard code -- switch back to userLocationObj
+          var costanza = mapFuncs.makeMarker(false, mapCanvas, mapOptions.center, 'George Costanza');
 
           geocoder.geocodeAddress(address).then(function(res) {
             $scope.searchAddress = res.formattedAddress;
@@ -85,20 +69,20 @@ angular.module('pottyParty.controllers')
           });
 
           console.log(currentLocation);
-          var someRestrooms = filterByRadius(currentLocation, allRestrooms, 0.0145);
+          // TEMPORARY HARD CODE -- swap back in currentLocation
+          var someRestrooms = mapFuncs.filterByRadius({
+            latitude: 40.754926,
+            longitude: -73.984281
+          }, allRestrooms, 0.0145);
 
           console.log(someRestrooms);
-          someRestrooms.forEach(function(el){
-             mapFuncs.makeMarker(true, mapCanvas, el);
+          someRestrooms.forEach(function(el) {
+            markers.push(mapFuncs.makeMarker(true, mapCanvas, el));
           });
-
-
+          makeClusters();
         }, function(err) {
           $cordovaToast.showLongCenter('Uh oh. The Potty Satellite 5000 seems to be out of orbit. Try again.');
         });
       };
-
-
-
     }
   ]);
--- pottyParty/www/js/services/map.service.js
+++ pottyParty/www/js/services/map.service.js
@@ -1,86 +1,95 @@
 angular.module('pottyParty.services')
-	.factory('mapFuncs', [function(){
-		return {
-			style: [
-		    {
-		        "featureType": "road",
-		        "elementType": "geometry",
-		        "stylers": [
-		            {
-		                "lightness": 100
-		            },
-		            {
-		                "visibility": "simplified"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "water",
-		        "elementType": "geometry",
-		        "stylers": [
-		            {
-		                "visibility": "on"
-		            },
-		            {
-		                "color": "#C6E2FF"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "poi",
-		        "elementType": "geometry.fill",
-		        "stylers": [
-		            {
-		                "color": "#C5E3BF"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "road",
-		        "elementType": "geometry.fill",
-		        "stylers": [
-		            {
-		                "color": "#D1D1B8"
-		            }
-		        ]
-		    }
-			],
-			makeMarker: function (rawCoords, map, data, title) {
-				var animation = null;
-				var position;
-				var icon;
+  .factory('mapFuncs', [
+    function() {
+      return {
+        style: [{
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [{
+            "lightness": 100
+          }, {
+            "visibility": "simplified"
+          }]
+        }, {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [{
+            "visibility": "on"
+          }, {
+            "color": "#C6E2FF"
+          }]
+        }, {
+          "featureType": "poi",
+          "elementType": "geometry.fill",
+          "stylers": [{
+            "color": "#C5E3BF"
+          }]
+        }, {
+          "featureType": "road",
+          "elementType": "geometry.fill",
+          "stylers": [{
+            "color": "#D1D1B8"
+          }]
+        }],
+        makeMarker: function(rawCoords, map, data, title) {
+          var animation = null;
+          var position;
+          var icon;
 
-				if (rawCoords) {
-					// raw data coordinates = [long, lat]
-					position = new google.maps.LatLng(data.coords[1], data.coords[0]);
-					title = data.name;
-				} else {
-					position = data;
-					animation = google.maps.Animation.DROP;
-				}
+          if (rawCoords) {
+            // raw data coordinates = [long, lat]
+            position = new google.maps.LatLng(data.coords[1], data.coords[0]);
+            title = data.name;
+          } else {
+            position = data;
+            animation = google.maps.Animation.DROP;
+          }
 
-				if (data.category === 'Coffee Shop') {
-					icon = 'img/coffee.png';
-				} else if (data.category === 'Public') {
-					icon = 'img/public.png';
-				} else if (data.category === 'Hotel') {
-					icon = 'img/hotel.png';
-				} else if (data.category === 'Book Store') {
-					icon = 'img/book.png';
-				} else if (data.category === 'Other') {
-					icon = 'img/other.png';
-				} else {
-					icon = 'img/costanza.png';
-				}
+          if (data.category === 'Coffee Shop') {
+            icon = 'img/coffee.png';
+          } else if (data.category === 'Public') {
+            icon = 'img/public.png';
+          } else if (data.category === 'Hotel') {
+            icon = 'img/hotel.png';
+          } else if (data.category === 'Book Store') {
+            icon = 'img/book.png';
+          } else if (data.category === 'Other') {
+            icon = 'img/other.png';
+          } else {
+            icon = 'img/costanza.png';
+          }
 
-				var marker = new google.maps.Marker({
-					position: position,
-					title: title,
-					map: map,
-					icon: icon,
-					animation: animation
-				});
-				return marker;
-			}
-		};
-	}]);
+          var marker = new google.maps.Marker({
+            position: position,
+            title: title,
+            map: map,
+            icon: icon,
+            animation: animation
+          });
+          return marker;
+        },
+        filterByRadius: function(origin, data, radius) {
+          // 0.0145 lat/lng
+          var c2 = radius * radius;
+          var xMin = origin.latitude - radius;
+          var xMax = origin.latitude + radius;
+          var yMin = origin.longitude - radius;
+          var yMax = origin.longitude + radius;
+          var filteredLocations = [];
+
+          data.forEach(function(place) {
+            if (place.coords[1] > xMax || place.coords[1] < xMin ||
+              place.coords[0] > yMax || place.coords[0] < yMin) {
+              return;
+            }
+            var a2 = (place.coords[1] - origin.latitude) * (place.coords[1] - origin.latitude);
+            var b2 = (place.coords[0] - origin.longitude) * (place.coords[0] - origin.longitude);
+            if (a2 + b2 <= c2) {
+              filteredLocations.push(place);
+            }
+          });
+          return filteredLocations;
+        }
+      };
+    }
+  ]);
